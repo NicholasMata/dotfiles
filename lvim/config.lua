eb66555572105187adbd2839657277f79b20c71b
Nicholas Mata <nicholas@matadesigns.net>
2023-10-09 07:51:11 -0700

    Improvements to kitty, zsh, and lunarvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,21 +1,64 @@
 lvim.colorscheme = "catppuccin-mocha"
 lvim.transparent_window = true
+vim.opt.wrap = true
 vim.opt.relativenumber = true
 
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+	-- for input mode
+	i = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+		["<C-n>"] = actions.cycle_history_next,
+		["<C-p>"] = actions.cycle_history_prev,
+	},
+	-- for normal mode
+	n = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+	},
+}
+
+-- Disable toggle terminal because I use kitty multiplexer
+-- lvim.builtin.terminal.active = false
+-- Prefer leader -> f to find project file which shows a preview.
 lvim.builtin.which_key.mappings["f"] = {
   require("lvim.core.telescope.custom-finders").find_project_files,
   "Find File"
 }
 
 lvim.plugins = {
-	{ 'rose-pine/neovim', name = 'rose-pine' },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'Hoffs/omnisharp-extended-lsp.nvim', lazy = true, ft = "cs" },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = { 'tpope/vim-dadbod' }
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine'
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    'Hoffs/omnisharp-extended-lsp.nvim',
+    lazy = true,
+    ft = "cs"
+  },
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function() require"lsp_signature".on_attach() end,
+    config = function() require "lsp_signature".on_attach() end,
   },
   { 'theprimeagen/vim-be-good' },
 }
 
+require("hardtime").setup()
