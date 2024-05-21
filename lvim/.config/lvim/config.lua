24be07dc57725dd6eb8bfdf3bd02d8ed4ef60a4a
Nicholas Mata <nicholas@matadesigns.net>
2024-05-21 06:33:31 -0700

    Improvements to lvim dependencies

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -4,57 +4,93 @@ reload("user.formatters")
 reload("user.linters")
 
 lvim.plugins = {
+  -- Better File Explorer
   {
-    -- NPM Packge Info
+    'stevearc/oil.nvim',
+    opts = {
+      view_options = {
+        show_hidden = true
+      }
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- NPM Packge Info
+  {
     "vuki656/package-info.nvim",
     dependencies = "MunifTanjim/nui.nvim",
   },
+
+  -- Markdown Preview
   {
-    -- Markdown Preview
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
+
+  -- View colors in editor
   {
-    -- View colors in editor
-    'NvChad/nvim-colorizer.lua'
+    'NvChad/nvim-colorizer.lua',
+    config = true
   },
+
+  -- Auto change vim theme
   {
-    -- Auto change vim theme
-    'f-person/auto-dark-mode.nvim'
+    'f-person/auto-dark-mode.nvim',
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option('background', 'dark')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option('background', 'light')
+      end,
+    }
   },
+
+  -- Theme
   {
-    -- Theme
     'rose-pine/neovim',
-    name = 'rose-pine'
+    name = 'rose-pine',
+    config = true
   },
+
+  -- Database Management
   {
-    -- Database Management
     'kristijanhusak/vim-dadbod-ui',
     dependencies = { 'tpope/vim-dadbod' }
   },
+
   -- CSharp
   {
     'nicholasmata/nvim-dap-cs',
+    dir = "$HOME/Developer/nvim-dap-cs",
+    dev = { true },
+    config = true,
+    ft = "cs",
     dependencies = { 'mfussenegger/nvim-dap' }
   },
+
   { 'Hoffs/omnisharp-extended-lsp.nvim' },
+
   -- Misc
   {
     'kkoomen/vim-doge',
     build = ":call doge#install()"
   },
+
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require "lsp_signature".on_attach() end,
   },
+
   { 'theprimeagen/vim-be-good' },
+
   { 'theprimeagen/harpoon' },
 
+  { 'tamton-aquib/keys.nvim' },
 }
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "csharp_ls" })
-
-reload("dap-cs").setup()
--- lvim/.config/lvim/lua/user/options.lua
+++ lvim/.config/lvim/lua/user/options.lua
@@ -1,5 +1,4 @@
 vim.opt.termguicolors = true
-require('rose-pine').setup()
 
 lvim.colorscheme = "rose-pine"
 lvim.transparent_window = true
@@ -16,18 +15,3 @@ lvim.builtin.nvimtree.setup.view = { relativenumber = true }
 vim.diagnostic.config({ virtual_text = false })
 
 lvim.builtin.telescope.pickers = { find_files = { hidden = true } }
-
-
-local auto_dark_mode = require('auto-dark-mode')
-
-auto_dark_mode.setup({
-  update_interval = 1000,
-  set_dark_mode = function()
-    vim.api.nvim_set_option('background', 'dark')
-  end,
-  set_light_mode = function()
-    vim.api.nvim_set_option('background', 'light')
-  end,
-})
-
-require 'colorizer'.setup()
