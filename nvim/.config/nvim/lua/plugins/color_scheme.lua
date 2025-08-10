34bc70b0b9929964ab17b8cc563e1cd409560cdf
Nicholas Mata <nicholas@matadesigns.net>
2025-08-10 13:31:26 -0700

    Make fidget background transparent, cleanup color scheme code

--- nvim/.config/nvim/lua/plugins/color_scheme.lua
+++ nvim/.config/nvim/lua/plugins/color_scheme.lua
@@ -1,3 +1,9 @@
+local set_transparent = function()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+end
+
 return {
 	-- Auto change vim theme on mode
 	{
@@ -6,15 +12,11 @@ return {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.api.nvim_set_option("background", "dark")
-				vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-				vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-				vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+				set_transparent()
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option("background", "light")
-				vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-				vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-				vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+				set_transparent()
 			end,
 		},
 	},
@@ -25,6 +27,7 @@ return {
 		config = true,
 		init = function()
 			vim.cmd.colorscheme("rose-pine")
+			set_transparent()
 		end,
 	},
 }
