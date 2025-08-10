04bba539d7949203392b82a451da945e7fc92e92
Nicholas Mata <nicholas@matadesigns.net>
2025-08-10 13:29:22 -0700

    Change ai model from ollama to openai which uses API key from Keychain

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,7 +1,19 @@
-local adapter = {
-	adapter = "ollama",
-	model = "codellama:7b",
+-- Default: OpenAI GPT-5
+local gpt5_strategy = {
+	adapter = {
+		name = "openai",
+		model = "gpt-5", -- make sure your account has access to GPT-5
+	},
 }
+
+-- Optional: Ollama (manual use)
+local ollama_strategy = {
+	adapter = {
+		name = "ollama",
+		model = "codellama:7b",
+	},
+}
+
 return {
 	"olimorris/codecompanion.nvim",
 	dependencies = {
@@ -11,6 +23,7 @@ return {
 	config = function(_, opts)
 		require("codecompanion").setup(opts)
 
+		-- Keymaps
 		vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap(
@@ -27,15 +40,40 @@ return {
 		)
 		vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
-		-- Expand 'cc' into 'CodeCompanion' in the command line
+		-- Expand 'cc' in command-line
 		vim.cmd([[cab cc CodeCompanion]])
 	end,
 	opts = {
+		-- Default strategies: GPT-5
 		strategies = {
-			chat = adapter,
-			inline = adapter,
-			agent = adapter,
+			chat = gpt5_strategy,
+			inline = gpt5_strategy,
+			agent = gpt5_strategy,
 		},
+
+		adapters = {
+			-- OpenAI GPT-5
+			openai = function()
+				return require("codecompanion.adapters").extend("openai", {
+					env = {
+						api_key = [[cmd:security find-generic-password -a openai -s OPENAI_API_KEY -w]],
+					},
+					schema = {
+						model = { default = "gpt-5" },
+					},
+				})
+			end,
+
+			-- Ollama (manual use)
+			ollama = function()
+				return require("codecompanion.adapters").extend("ollama", {
+					schema = {
+						model = { default = ollama_strategy.adapter.model },
+					},
+				})
+			end,
+		},
+
 		opts = {
 			log_level = "DEBUG",
 		},
