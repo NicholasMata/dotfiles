b2e593d14beacdb78516cd3ce21eddcc6b4c3d33
Nicholas Mata <nicholas@matadesigns.net>
2025-08-10 09:28:12 -0700

    Add text to speech plugin and remove local implementation of tts

--- nvim/.config/nvim/lua/plugins/which_key.lua
+++ nvim/.config/nvim/lua/plugins/which_key.lua
@@ -4,30 +4,19 @@ return {
 		"folke/which-key.nvim",
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
-			local which_key = require("which-key")
-			which_key.setup()
+			local wk = require("which-key")
+			wk.setup()
 
-			-- Document existing key chains
-			which_key.add({
-				{ "<leader>m", group = "[m]arkdown" },
-				{ "<leader>mp", group = "[p]review" },
-				{ "<leader>l", group = "[l]sp" },
-				{ "<leader>d", group = "[d]ebug" },
-				{ "<leader>t", group = "[t]elescope" },
-				{ "<leader>g", group = "[g]it" },
-				{ "<leader>o", group = "[o]s" },
-				{ "<leader>v", ":call system('say '.shellescape(expand('<cword>')).' &')<CR>", name = "[v]oice word" },
+			-- Declare leader groups (no rhs)
+			wk.add({
+				{ "m", group = "[m]arkdown" },
+				{ "mp", group = "[p]review" },
+				{ "l", group = "[l]sp" },
+				{ "d", group = "[d]ebug" },
+				{ "t", group = "[t]elescope" },
+				{ "g", group = "[g]it" },
+				{ "o", group = "[o]s" },
 			}, { mode = "n", prefix = "<leader>" })
-			-- visual mode
-			--
-			-- lvim.builtin.which_key.mappings["v"] = { 'Speak Word' }
-			-- lvim.builtin.which_key.vmappings["v"] = { 'Speak Selection' }
-			-- lvim.keys.visual_mode["<leader>v"] = { '"xy:call system(\'say \'. shellescape(@x) .\' &\')<CR>', desc = 'Speak' }
-			-- lvim.keys.normal_mode["<leader>v"] = ":call system('say '.shellescape(expand('<cword>')).' &')<CR>"
-			-- which_key.register({
-			--   h = { 'Git [H]unk' },
-			--   v = { '"xy:call system(\'say \'. shellescape(@x) .\' &\')<CR>', 'Speak Selection' }
-			-- }, { mode = 'v', prefix = '<leader>' })
 		end,
 	},
 }
