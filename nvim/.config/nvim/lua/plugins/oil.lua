231ad5cafe8afb74d5541f6aa0ad378de15a3de7
Nicholas Mata <nicholas@matadesigns.net>
2026-04-04 10:48:58 -0700

    add shortcut to oil for toggling icons

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -13,6 +13,22 @@ return {
       "<cmd>Oil<cr>",
       desc = "[e]xplorer",
     },
+    {
+      "<leader>oi",
+      function()
+        local oil = require("oil")
+
+        -- Track icon state globally
+        vim.g.oil_icons_enabled = not vim.g.oil_icons_enabled
+
+        if vim.g.oil_icons_enabled then
+          oil.set_columns({ "icon" })
+        else
+          oil.set_columns({})
+        end
+      end,
+      desc = "Oil: Toggle filetype icons",
+    },
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
