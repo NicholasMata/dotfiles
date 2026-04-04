f02f6cd60941a18a29573fd1cdaa8c0fff119c92
Nicholas Mata <nicholas@matadesigns.net>
2026-04-04 10:48:03 -0700

    switch from vale to markdownlint-cli2

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        markdown = { "vale" },
+        markdown = { "markdownlint-cli2" },
         json = { "jsonlint" },
         text = { "vale" },
         typescriptreact = { "eslint_d" },
