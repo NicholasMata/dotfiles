effd09b706d0e40c6e57e84ad9b4bc81b44fd43c
Nicholas Mata <nicholas@matadesigns.net>
2026-07-19 20:22:42 -0700

    Add Herdr

---
+++ nvim/.config/nvim/lua/plugins/multiplexer.lua
@@ -0,0 +1,10 @@
+return {
+  "paulbkim-dev/vim-herdr-navigation",
+  lazy = false,
+  dependencies = {
+    "christoomey/vim-tmux-navigator",
+  },
+  config = function(plugin)
+    dofile(plugin.dir .. "/editor/nvim.lua")
+  end,
+}
